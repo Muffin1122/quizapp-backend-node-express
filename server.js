0c826f71f5a6bcbeb8c86800d677fe503fb07cd0
Muffin1122 <bscs2211@gmail.com>
2024-05-16 15:47:58 +0500

    commit

--- server.js
+++ server.js
@@ -38,44 +38,6 @@ app.use('/api/question', questionRouter);
 app.use('/api/discussion',discussionRouter );
 app.use('/api/uploads', fileRouter);
 
-// ----------------------------MULTER--------------
-
-// const multer = require("multer");
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "./files");
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now();
-//     cb(null, uniqueSuffix + file.originalname);
-//   },
-// });
-
-// require("./server/models/fileSchema");
-// const PdfSchema = mongoose.model("PdfDetails");
-
-// const upload = multer({ storage: storage });
-// app.post("/upload-files", upload.single("file"), async (req, res) => {
-//   console.log(req.file);
-//   const title = req.body.title;
-//   const fileName = req.file.filename;
-//   try {
-//     await PdfSchema.create({ title: title, pdf: fileName });
-//     res.send({ status: "ok" });
-//   } catch (error) {
-//     res.json({ status: error });
-//   }
-// });
-// app.get("/get-files", async (req, res) => {
-//   try {
-//     PdfSchema.find().then((data) => {
-//       res.send({  documents: data });
-//     });
-//   } catch (error) {}
-// });
-
-
 
 
   // Server Connection
--- server/controllers/discussionController.js
+++ server/controllers/discussionController.js
@@ -12,14 +12,60 @@ exports.createDiscussion = catchAsync(async (req, res, next) => {
   });
 });
 
+// exports.getAllDiscussions = catchAsync(async (req, res, next) => {
+//   const discussion = await Discussion.find();
+//   if (!discussion) {
+//     return next(new AppError('No discussion found ', 404));
+//   }
+//   res.json({
+//     data: {
+//       discussion
+//     }
+//   })
+// });
+
 exports.getAllDiscussions = catchAsync(async (req, res, next) => {
-  const discussion = await Discussion.find();
-  if (!discussion) {
-    return next(new AppError('No discussion found ', 404));
+  const discussions = await Discussion.aggregate([
+    {
+      $lookup: {
+        from: 'users', 
+        localField: 'author',
+        foreignField: '_id',
+        as: 'author'
+      }
+    },
+    {
+      $unwind: '$author'
+    },
+    {
+      $lookup: {
+        from: 'quizzes', 
+        localField: 'quiz',
+        foreignField: '_id',
+        as: 'quiz'
+      }
+    },
+    {
+      $unwind: '$quiz'
+    },
+    {
+      $project: {
+        _id: 0,
+        comments: 1,
+        author: '$author.name', 
+        quizTitle: '$quiz.title',
+        createdAt: 1,
+      }
+    }
+  ]);
+  
+  if (!discussions || discussions.length === 0) {
+    return next(new AppError('No discussions found', 404));
   }
+
   res.json({
     data: {
-      discussion
+      discussion: discussions
     }
-  })
+  });
 });
